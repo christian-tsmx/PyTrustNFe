c2fc2552bcf06d9c07c8e2aed754bb0804852b29
Christian David <christian@tsmx.net.br>
2023-07-26 15:34:39 -0300

    #569 - commit inicial

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ later (LGPLv2+)",
             "nfse/issweb/templates/*xml",
             "nfse/speedgov/templates/*xml",
             "nfse/webiss/templates/*xml",
+            "nfse/tributus/templates/*xml",
             "xml/schemas/*xsd",
         ]
     },
